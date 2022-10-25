3bb3c9a0a977beb0d20f0ba4759039c0518949b6
catzy007 <catzy.rais@gmail.com>
2022-10-25 15:22:18 +0700

    refactor

---
+++ refactor/statuschecker.c
@@ -0,0 +1,40 @@
+#include "statuschecker.h"
+
+int isDevicePaired(){
+    const char checkstring1[] = "List";
+    const char checkstring2[] = "device";
+    FILE *buffer;
+    char text[255];
+    int status = 0;
+    char *token;
+
+    buffer = popen("adb devices", "r");
+    if(buffer == NULL){
+		return -1;
+	}
+
+	while(fgets(text, sizeof(text), buffer) != NULL){
+        // // printf("%s\n", text);
+        // token = strtok(text, " ");
+        // while( token != NULL ) {
+        //     // printf("%s\n", token);
+        //     if(strstr(token, checkstring1) != NULL){
+        //         printf("Found : %s\n", token);
+        //         status = 0;
+        //     }
+        //     token = strtok(NULL, " ");
+        // }
+        token = strtok(text, "\t");
+        while(token != NULL) {
+            // printf("%s\n", token);
+            if(strstr(token, checkstring1) == NULL && 
+                strstr(token, checkstring2) != NULL){
+                printf("Found : %s\n", token);
+                status = 1;
+            }
+            token = strtok(NULL, "\t");
+        }
+	}
+	pclose(buffer);
+    return status;
+}
---
+++ refactor/statuschecker.h
@@ -0,0 +1,9 @@
+#ifndef STATUSCHECKER_H_INCLUDED
+#define STATUSCHECKER_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int isDevicePaired();
+
+#endif
