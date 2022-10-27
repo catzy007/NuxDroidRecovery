c7ace4bad11390f0e8d02d1fd0a3ddaf6474bffb
catzy007 <catzy.rais@gmail.com>
2022-10-27 14:29:46 +0700

    refactor

--- refactor/main.c
+++ refactor/main.c
@@ -18,6 +18,8 @@ int main(int argc, char** argv){
         partitionCopy(value);
     }
 
+    partitionExtractor();
+
     free(value);
     return 0;
 }
--- refactor/partitionselector.c
+++ refactor/partitionselector.c
@@ -59,3 +59,61 @@ char *partitionSelector(){
 
     return partitionName;
 }
+
+int partitionExtractor(){
+    FILE *buffer;
+    char text[255];
+    char command[255];
+    char *token, *ptr;
+
+    char startSector[50] = "0\0";
+    char endSector[50] = "0\0";
+    char tempStartSector[50] = "0\0";
+    char tempEndSector[50] = "0\0";
+    unsigned long int largestSector = 0;
+    unsigned long int tempLargestSector = 0;
+
+    //sfdisk -q -l -uS *.img 
+    buffer = popen("sfdisk -q -l -uS *.img", "r");
+    // buffer = popen("sfdisk -q -l -uS", "r");
+    if(buffer == NULL){
+        return -1;
+    }
+
+    while(fgets(text, sizeof(text), buffer) != NULL){
+        token = strtok(text, " ");
+        int i = 0;
+        while(token != NULL){
+            if(i == 1){
+                strcpy(tempStartSector, token);
+                token = strtok(NULL, " ");
+                strcpy(tempEndSector, token);
+                token = strtok(NULL, " ");
+                tempLargestSector = strtoul(token, &ptr, 10);
+                if(tempLargestSector > largestSector){
+                    largestSector = tempLargestSector;
+                    strcpy(startSector, tempStartSector);
+                    strcpy(endSector, tempEndSector);
+                }
+            }
+            // printf("%s\n", token);
+            token = strtok(NULL, " ");
+            i++;
+        }
+    }
+    pclose(buffer);
+
+    printf("start %s, end %s, sector %ld\n", startSector, endSector, largestSector);
+    if(largestSector == 0){
+        return 0;
+    }
+
+    strcpy(command, "dd if=deviceImage.img of=deviceImage.dd skip=");
+    strcat(command, startSector);
+	strcat(command, " count=");
+	strcat(command, endSector);
+    printf("%s\n", command);
+    // system(command);
+
+    return 1;
+}
